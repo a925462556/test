662b362efa7a639a3ee3cf998f1064611314803a
swc <swc@123.com>
2019-12-27 13:38:11 +0800

    test2

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("my first github class");
         System.out.println("new");
+        System.out.println("new two");
     }
 }
